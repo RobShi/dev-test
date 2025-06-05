7c1950dc540e8e78b2a418b2f31e6c84663cdd7d
Rob Whittemore <Rob_Whittemore@SHI.com>
2025-06-05 15:14:28 -0400

    Added a 'unit test'

--- csharpcore/ShiMartTest/UnitTest1.cs
+++ csharpcore/ShiMartTest/UnitTest1.cs
@@ -8,77 +8,35 @@ namespace ShiMartTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void VerifyItemBehaviors()
         {
-            var items = new List<Item> { new Item { Name = "Test", SellIn = 10, Quality = 20 } };
-            var app = new ShiMart(items);
-
-            app.UpdateQuality();
-
-            Assert.Equal(9, items.First().SellIn);
-            Assert.Equal(19, items.First().Quality);
-        }
-
-        [Fact]
-        public void RegularItem_DecreasesQualityByOne_WhenSellInPositive()
-        {
-            // Arrange
-            var item = new Item { Name = "Regular Item", SellIn = 5, Quality = 10 };
-            var items = new List<Item> { item };
-            var shimart = new ShiMart(items);
-
-            // Act
-            shimart.UpdateQuality();
-
-            // Assert
-            Assert.Equal(4, item.SellIn);
-            Assert.Equal(9, item.Quality);
-        }
-
-        [Fact]
-        public void AgedBrie_IncreasesQualityByOne_WhenSellInPositive()
-        {
-            var item = new Item() { Name = "Aged Brie", SellIn = 5, Quality = 10 };
-            var items = new List<Item>() { item };
-            var app = new ShiMart(items);
+            var testData = new List<(Item item, int expectedSellIn, int expectedQuality)>
+            {
+                (new Item { Name = "Test", SellIn = 10, Quality = 20 }, 9, 19), (new Item { Name = "Regular Item", SellIn = 5, Quality = 10 }, 4, 9), (new Item { Name = "Aged Brie", SellIn = 5, Quality = 10 }, 4, 11)
+            };
             
+            var x = testData.Select(t => t.item).ToList();
+            x.Add(new Item { Name = "Some Item", SellIn = 10, Quality = 20 });
+            x.Add(new Item { Name = "Aged Brie", SellIn = 10, Quality = 20 });
+            var y = new Item { Name = "Canned Beans", SellIn = 10, Quality = 20 };
+            x.Add(y);
+            
+            var app = new ShiMart(x);
             app.UpdateQuality();
             
-            Assert.Equal(4, items[0].SellIn);
-            Assert.Equal(11, items[0].Quality);
-        }
-
-        [Fact]
-        public void TestMultipleItemsAtOnce()
-        {
-            var regularItem = new Item { Name = "Some Item", SellIn = 10, Quality = 20 };
-            var agedBrie = new Item { Name = "Aged Brie", SellIn = 10, Quality = 20 };
-            var items = new List<Item> { regularItem, agedBrie };
+            for (int i = 0; i < testData.Count; i++)
+            {
+                Assert.Equal(testData[i].expectedSellIn, testData[i].item.SellIn);
+                Assert.Equal(testData[i].expectedQuality, testData[i].item.Quality);
+            }
             
-            var app = new ShiMart(items);
-            app.UpdateQuality();
+            Assert.Equal(9, x[3].SellIn);
+            Assert.Equal(19, x[3].Quality);
+            Assert.Equal(9, x[4].SellIn);
+            Assert.Equal(21, x[4].Quality);
             
-            // regular item goes down
-            Assert.Equal(9, regularItem.SellIn);
-            Assert.Equal(19, regularItem.Quality);
-            
-            // aged brie goes up
-            Assert.Equal(9, agedBrie.SellIn);
-            Assert.Equal(21, agedBrie.Quality);
-        }
-
-        [Fact] 
-        public void CannedBeans_DoesntChange()
-        {
-            var beans = new Item { Name = "Canned Beans", SellIn = 10, Quality = 20 };
-            List<Item> items = new List<Item>();
-            items.Add(beans);
-            ShiMart app = new ShiMart(items);
-            
-            app.UpdateQuality();
-            
-            Assert.Equal(10, beans.SellIn); // doesn't decrease
-            Assert.Equal(20, beans.Quality); // doesn't decrease
+            Assert.Equal(10, y.SellIn);
+            Assert.Equal(20, y.Quality);
         }
     }
 }
